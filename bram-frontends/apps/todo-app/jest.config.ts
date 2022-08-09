4bd5d912c7f21e10bfbddb07a5c3be0752e9e19b
brambooo <bram_reinold@hotmail.com>
2022-08-09 07:37:30 +0200

    first commit

---
+++ bram-frontends/apps/todo-app/jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'todo-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '.*.(vue)$': '@vue/vue2-jest',
+    '^.+.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  coverageDirectory: '../../coverage/apps/todo-app',
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+  ],
+  moduleNameMapper: {
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    'vue-jest': { tsConfig: 'apps/todo-app/tsconfig.spec.json' },
+  },
+};
